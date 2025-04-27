d16c40e5f7d5386ceec288943a81459286d4674a
walaamadan20 <walaamadan@outlook.com>
2025-04-27 20:59:29 +0300

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,9 +3,7 @@ import { authContext } from '../context/AuthContext'
 import axios from 'axios'
 
 function Homepage() {
-  // useContext(): allows me to consume the context
 
-  // sending request to protected route that needs a token
   async function callProtectedRoute(){
     const token = localStorage.getItem("token")
     const response= await axios.get(`${import.meta.env.VITE_BACKEND_URL}/test-jwt/checkout`,{headers:{Authorization:`Bearer ${token}`}})
